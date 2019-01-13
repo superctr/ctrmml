9763483219212ee28293a37ea642ed6973d1dea8
ian karlsson <themvrules@gmail.com>
2019-01-13 12:07:11 +0100

    refactor mml.c

--- mml.c
+++ mml.c
@@ -5,231 +5,229 @@
 #include <ctype.h>
 #include "ctrmml.h"
 
-// todo: move these to struct
-// globals for the MML module
-	static FILE* f;
-	static char* fn; // filename
-	static int line; // current line
-	static int line_pos; // ftell at the beginning of the line
+struct mml_file {
+	FILE* file;
+	char* filename; // filename
+	int line; // current line
+	int line_pos; // ftell at the beginning of the line
 
-	static char buffer[1024];
+	char buffer[1024];
 
-	static struct song *song; // working area
-	static void (*last_cmd)();
+	struct song *song; // working area
+	void (*last_cmd)();
 
-	static struct track *trk;
-	static int trk_id;
-	static int trk_offset; // will be used for the {} command
-	static char trk_list[256]; // track batching
-	static int trk_count; // tracks in the batch list
+	struct track *trk;
+	int trk_id;
+	int trk_offset; // will be used for the {} command
+	char trk_list[256]; // track batching
+	int trk_count; // tracks in the batch list
+};
 
-#define PARSE_ERROR(msg) error(msg, 1, 1)
-#define PARSE_WARN(msg) error(msg, 1, 0)
-#define ERROR(msg) error(msg, 0, 1)
-#define WARN(msg) error(msg, 0, 0)
-static void error(char* error_msg, int column, int fatal)
+#define PARSE_ERROR(mml,msg) error(mml, msg, 1, 1)
+#define PARSE_WARN(mml,msg) error(mml, msg, 1, 0)
+#define ERROR(mml,msg) error(mml, msg, 0, 1)
+#define WARN(mml,msg) error(mml, msg, 0, 0)
+static void error(struct mml_file *mml, char* error_msg, int column, int fatal)
 {
-	int curr_pos = ftell(f);
 	char* error_s = fatal ? "Error: " : "Warning: ";
-	fseek(f, line_pos, SEEK_SET);
-	fgets(buffer, 1024, f);
+	int error_column = ftell(mml->file) - mml->line_pos;
+	fseek(mml->file, mml->line_pos, SEEK_SET);
+	fgets(mml->buffer, 1024, mml->file);
 	if(column)
-		fprintf(stderr,"%s:%d:%d: %s%s\n", fn, line, curr_pos-line_pos, error_s, error_msg);
+		fprintf(stderr,"%s:%d:%d: %s%s\n", mml->filename, mml->line, error_column, error_s, error_msg);
 	else
-		fprintf(stderr,"%s:%d: %s%s\n", fn, line, error_s, error_msg);
-	fprintf(stderr,"%s", buffer);
+		fprintf(stderr,"%s:%d: %s%s\n", mml->filename, mml->line, error_s, error_msg);
+	fprintf(stderr,"%s", mml->buffer);
 	// add a newline if missing
-	if(strlen(buffer) > 0 && buffer[strlen(buffer)-1] != '\n')
+	if(strlen(mml->buffer) > 0 && mml->buffer[strlen(mml->buffer)-1] != '\n')
 		putc('\n', stderr);
 	if(column)
 	{
 		// display an arrow pointing to the problematic column
 		column = 0;
-		while(column < curr_pos-line_pos-1)
-			putc((buffer[column++] == '\t') ? '\t' : ' ', stderr);
+		while(column < error_column-1)
+			putc((mml->buffer[column++] == '\t') ? '\t' : ' ', stderr);
 		fputs("^\n",stderr);
 	}
 	if(fatal)
 		exit(-1);
 	else
-		fseek(f, curr_pos, SEEK_SET);
+		fseek(mml->file, error_column + mml->line_pos, SEEK_SET);
 }
 
 // return 1 if eol (or EOF). should handle windows newlines as well (untested yet)
-static int iseol(int c)
+static int iseol(struct mml_file* mml, int c)
 {
 	if(c == '\r')
-		c = getc(f);
+		c = getc(mml->file);
 	if(c == '\n' || c == EOF)
-	{
 		return 1;
-	}
 	return 0;
 }
 
 // return first non-blank character
-static int my_getc(FILE *f)
+static int my_getc(struct mml_file* mml)
 {
 	int c;
-	do c = getc(f);
+	do c = getc(mml->file);
 	while(isblank(c));
 	return c;
 }
 
-static int my_ungetc(int c, FILE *f)
+static int my_ungetc(int c, struct mml_file* mml)
 {
-	return ungetc(c,f);
+	return ungetc(c,mml->file);
 }
 
 // wrapper around scanf to make sure newlines are not skipped
 // also allows hex prefix
-static int scannum(int* num)
+static int scannum(struct mml_file* mml, int* num)
 {
-	int c = my_getc(f); // skips spaces, but not newlines
+	int c = my_getc(mml); // skips spaces, but not newlines
 	if(isdigit(c))
 	{
-		my_ungetc(c,f);
-		return fscanf(f,"%d",num);
+		my_ungetc(c,mml);
+		return fscanf(mml->file,"%d",num);
 	}
 	else if(c == '$' || c == 'x')
 	{
-		return fscanf(f,"%x",num);
+		return fscanf(mml->file,"%x",num);
 	}
 	else
 	{
-		my_ungetc(c,f);
+		my_ungetc(c,mml);
 		return -1;
 	}
 }
 
 // Parser functions
 // Read duration, if none found use the default value from the track
-static int get_duration()
+static int get_duration(struct mml_file* mml)
 {
 	int duration = 0, c = 0;
-	if(scannum(&duration) != 1)
-		duration = trk->duration;
+	if(scannum(mml, &duration) != 1)
+		duration = mml->trk->duration;
 	else
-		duration = trk->measure_len / duration;
-	c = my_getc(f);
+		duration = mml->trk->measure_len / duration;
+	c = my_getc(mml);
 	if(c == '.') // dotted duration
 		duration += duration>>1;
 	else
-		my_ungetc(c, f);
+		my_ungetc(c, mml);
 
 	return duration;
 }
 
 // Read parameter
-static int parameter(int default_param)
+static int parameter(struct mml_file* mml, int default_param)
 {
-	scannum(&default_param);
+	scannum(mml, &default_param);
 	return default_param;
 }
 
 // Read parameter and throw error if none found
-static int expect_parameter()
+static int expect_parameter(struct mml_file* mml)
 {
 	int param;
-	if(scannum(&param) == 1)
+	if(scannum(mml, &param) == 1)
 		return param;
 	else
-		PARSE_ERROR("missing parameter");
+		PARSE_ERROR(mml,"missing parameter");
 }
 
 // Read signed parameter
-static int expect_signed()
+static int expect_signed(struct mml_file* mml)
 {
-	int param, c=my_getc(f);
+	int param, c=my_getc(mml);
 	if(c != '+' && c != '-')
-		my_ungetc(c,f);
-	if(scannum(&param) == 1)
+		my_ungetc(c,mml);
+	if(scannum(mml, &param) == 1)
 	{
 		if(c == '-')
 			param = -param;
 		return param;
 	}
 	else
-		PARSE_ERROR("missing parameter");
+		PARSE_ERROR(mml,"missing parameter");
 }
 
 // Convert note and read sharps / flats
-static int note(int c)
+static int note(struct mml_file* mml, int c)
 {
 	static const int note_values[8] = {9,11,0,2,4,5,7,11}; // abcdefgh
 	int val = c - 'a';
-	if(!track_in_drum_mode(trk))
+	if(!track_in_drum_mode(mml->trk))
 		val = note_values[val];
-	c = my_getc(f);
+	c = my_getc(mml);
 	if(c == '+') // sharp
 		val++;
 	else if(c == '-') // flat
 		val--;
 	else
-		my_ungetc(c, f);
+		my_ungetc(c, mml);
 	return val;
 }
 
 // Handlers for MML commands
-static void mml_slur()
+static void slur(struct mml_file *mml)
 {
-	if(track_slur(trk))
-		PARSE_WARN("slur may not affect articulation of previous note");
+	if(track_slur(mml->trk))
+		PARSE_WARN(mml, "slur may not affect articulation of previous note");
 }
 
-static void mml_reverse_rest(int duration)
+static void reverse_rest(struct mml_file *mml, int duration)
 {
-	int status = track_reverse_rest(trk, duration);
+	int status = track_reverse_rest(mml->trk, duration);
 	if(status == -1)
-		PARSE_ERROR("unable to modify previous duration (insert a loop break?)");
+		PARSE_ERROR(mml, "unable to modify previous duration (insert a loop break?)");
 	else if(status == -2)
-		PARSE_ERROR("command duration too long (delete previous note?");
+		PARSE_ERROR(mml, "command duration too long (delete previous note?");
 }
 
-static void mml_grace()
+static void grace(struct mml_file *mml)
 {
-	int c = note(my_getc(f));
-	int duration = get_duration();
-	mml_reverse_rest(duration);
-	track_note(trk, c, duration);
+	int c = note(mml, my_getc(mml));
+	int duration = get_duration(mml);
+	reverse_rest(mml, duration);
+	track_note(mml->trk, c, duration);
 }
 
 // combination command that allows for two atom_types depending on
 // if sign prefix is found.
-static void mml_atom_relative(enum atom_command type, enum atom_command subtype)
+static void atom_relative(struct mml_file *mml, enum atom_command type, enum atom_command subtype)
 {
-	int param, c=my_getc(f);
+	int param, c=my_getc(mml);
 	if(c == '+' || c == '-')
 		type == subtype;
 	else
-		my_ungetc(c,f);
-	if(scannum(&param) == 1)
+		my_ungetc(c,mml);
+	if(scannum(mml, &param) == 1)
 	{
 		if(type == ATOM_CMD_INVALID)
-			PARSE_ERROR("parameter must be relative (+ or - prefix)");
+			PARSE_ERROR(mml, "parameter must be relative (+ or - prefix)");
 		if(c == '-')
 			param = -param;
-		track_atom(trk, type, param);
+		track_atom(mml->trk, type, param);
 	}
 	else
-		PARSE_ERROR("missing parameter");
+		PARSE_ERROR(mml,"missing parameter");
 }
 
 // read MML commands
-static void parse_mml(int conditional_block)
+static void parse_mml(struct mml_file *mml, int conditional_block)
 {
 	int c;
 
 	if(conditional_block)
 	{
-		int offset = trk_offset;
+		int offset = mml->trk_offset;
 		while(offset)
 		{
 			// skip to break
-			while(!iseol(c) && c != '/' && c != ';')
-				c = getc(f);
+			do c = getc(mml->file);
+			while(!iseol(mml, c) && c != '/' && c != ';');
 			if(c != '/')
-				ERROR("unterminated conditional block");
+				ERROR(mml, "unterminated conditional block");
 			c = 0;
 			offset--;
 		}
@@ -237,7 +235,7 @@ static void parse_mml(int conditional_block)
 
 	while(1)
 	{
-		c = my_getc(f);
+		c = my_getc(mml);
 
 		if(c == '|')
 			continue;
@@ -245,109 +243,109 @@ static void parse_mml(int conditional_block)
 			break;
 
 		if(c >= 'a' && c <= 'h')
-			c = note(c), track_note(trk, c, get_duration());
+			c = note(mml, c), track_note(mml->trk, c, get_duration(mml));
 		else if(c == 'r')
-			track_rest(trk, get_duration());
+			track_rest(mml->trk, get_duration(mml));
 		else if(c == '^')
-			track_tie(trk, get_duration());
+			track_tie(mml->trk, get_duration(mml));
 		else if(c == '&')
-			mml_slur();
+			slur(mml);
 		else if(c == 'o')
-			track_octave(trk, expect_parameter());
+			track_octave(mml->trk, expect_parameter(mml));
 		else if(c == '<')
-			track_octave_down(trk);
+			track_octave_down(mml->trk);
 		else if(c == '>')
-			track_octave_up(trk);
+			track_octave_up(mml->trk);
 		else if(c == 'l')
-			trk->duration = get_duration();
+			mml->trk->duration = get_duration(mml);
 		else if(c == 'q')
-			track_quantize(trk, expect_parameter());
+			track_quantize(mml->trk, expect_parameter(mml));
 		else if(c == '@') // may handle alnum ids in the future...
-			track_atom(trk, ATOM_CMD_INS, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_INS, expect_parameter(mml));
 		else if(c == 'k')
-			track_atom(trk, ATOM_CMD_TRANSPOSE, expect_signed());
+			track_atom(mml->trk, ATOM_CMD_TRANSPOSE, expect_signed(mml));
 		else if(c == 'K')
-			track_atom(trk, ATOM_CMD_DETUNE, expect_signed());
+			track_atom(mml->trk, ATOM_CMD_DETUNE, expect_signed(mml));
 		else if(c == 'v')
-			track_atom(trk, ATOM_CMD_VOL, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_VOL, expect_parameter(mml));
 		else if(c == '(')
-			track_atom(trk, ATOM_CMD_VOL_REL, -parameter(1));
+			track_atom(mml->trk, ATOM_CMD_VOL_REL, -parameter(mml, 1));
 		else if(c == ')')
-			track_atom(trk, ATOM_CMD_VOL_REL, parameter(1));
+			track_atom(mml->trk, ATOM_CMD_VOL_REL, parameter(mml, 1));
 		else if(c == 'V')
-			mml_atom_relative(ATOM_CMD_VOL_FINE, ATOM_CMD_VOL_FINE_REL);
+			atom_relative(mml, ATOM_CMD_VOL_FINE, ATOM_CMD_VOL_FINE_REL);
 		else if(c == 'p')
-			track_atom(trk, ATOM_CMD_PAN, expect_signed());
+			track_atom(mml->trk, ATOM_CMD_PAN, expect_signed(mml));
 		else if(c == 'E')
-			track_atom(trk, ATOM_CMD_VOL_ENVELOPE, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_VOL_ENVELOPE, expect_parameter(mml));
 		else if(c == 'M')
-			track_atom(trk, ATOM_CMD_PITCH_ENVELOPE, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_PITCH_ENVELOPE, expect_parameter(mml));
 		else if(c == 'P')
-			track_atom(trk, ATOM_CMD_PAN_ENVELOPE, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_PAN_ENVELOPE, expect_parameter(mml));
 		else if(c == 'D')
-			track_drum_mode(trk, expect_parameter());
+			track_drum_mode(mml->trk, expect_parameter(mml));
 		else if(c == 't')
-			track_atom(trk, ATOM_CMD_TEMPO_BPM, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_TEMPO_BPM, expect_parameter(mml));
 		else if(c == 'T')
-			track_atom(trk, ATOM_CMD_TEMPO, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_TEMPO, expect_parameter(mml));
 		else if((c == '/' || c == '}') && conditional_block)
 			break;
 		else if(c == '{' && !conditional_block)
-			parse_mml(1); // enable conditional block
+			parse_mml(mml, 1); // enable conditional block
 		else if(c == '[')
-			track_atom(trk, ATOM_CMD_LOOP_START, 0);
+			track_atom(mml->trk, ATOM_CMD_LOOP_START, 0);
 		else if(c == '/')
-			track_atom(trk, ATOM_CMD_LOOP_BREAK, 0);
+			track_atom(mml->trk, ATOM_CMD_LOOP_BREAK, 0);
 		else if(c == ']')
-			track_atom(trk, ATOM_CMD_LOOP_END, parameter(2));
+			track_atom(mml->trk, ATOM_CMD_LOOP_END, parameter(mml, 2));
 		else if(c == 'L')
-			track_atom(trk, ATOM_CMD_SEGNO, 0);
+			track_atom(mml->trk, ATOM_CMD_SEGNO, 0);
 		else if(c == 'R')
-			mml_reverse_rest(get_duration());
+			reverse_rest(mml, get_duration(mml));
 		else if(c == 'G')
-			mml_grace();
+			grace(mml);
 		else if(c == '*')
-			track_atom(trk, ATOM_CMD_JUMP, expect_parameter());
+			track_atom(mml->trk, ATOM_CMD_JUMP, expect_parameter(mml));
 
-		else if(iseol(c))
+		else if(iseol(mml, c))
 			return;
 		else
-			PARSE_ERROR("unknown command");
+			PARSE_ERROR(mml, "unknown command");
 	}
 
 	if(conditional_block)
 	{
 		// skip to terminator
-		while(!iseol(c) && c != '}' && c != ';')
-			c = my_getc(f);
+		while(!iseol(mml, c) && c != '}' && c != ';')
+			c = my_getc(mml);
 		if(c != '}')
-			ERROR("unterminated conditional block");
+			ERROR(mml, "unterminated conditional block");
 		return;
 	}
 
 	// comment found, skipping to eol
-	while(!iseol(c))
-		c = my_getc(f);
+	while(!iseol(mml, c))
+		c = my_getc(mml);
 }
 
-static void parse_trk_list()
+static void parse_trk_list(struct mml_file *mml)
 {
-	int saved_line = line;
-	long file_pos = ftell(f);
+	int saved_line = mml->line;
+	long file_pos = ftell(mml->file);
 	int i;
 
-	if(!trk_count)
-		PARSE_ERROR("no track(s) specified");
+	if(!mml->trk_count)
+		PARSE_ERROR(mml, "no track(s) specified");
 
-	for(i = 0; i < trk_count; i++)
+	for(i = 0; i < mml->trk_count; i++)
 	{
-		fseek(f, file_pos, SEEK_SET);
-		trk_id = trk_list[i];
-		trk = song->track[trk_id];
-		track_enable(trk, trk_id);
-		line = saved_line;
-		trk_offset = i;
-		parse_mml(0);
+		fseek(mml->file, file_pos, SEEK_SET);
+		mml->trk_id = mml->trk_list[i];
+		mml->trk = mml->song->track[mml->trk_id];
+		track_enable(mml->trk, mml->trk_id);
+		mml->line = saved_line;
+		mml->trk_offset = i;
+		parse_mml(mml,0);
 	}
 }
 
@@ -363,77 +361,79 @@ static int get_track_id(char c)
 		return -2;
 }
 
-static void parse_line()
+static void parse_line(struct mml_file *mml)
 {
 	int c;
-	line++;
-	line_pos = ftell(f);
+	mml->line++;
+	mml->line_pos = ftell(mml->file);
 
-	c = getc(f);
+	c = getc(mml->file);
 	if(get_track_id(c) > -2)
 	{
 		int i = 0;
 		// read the channel mask
-		while((trk_id = get_track_id(c)) > -2)
+		while((mml->trk_id = get_track_id(c)) > -2)
 		{
-			if(trk_id == -1 && scannum(&trk_id) != 1)
-				PARSE_ERROR("could not parse track ID");
+			if(mml->trk_id == -1 && scannum(mml, &mml->trk_id) != 1)
+				PARSE_ERROR(mml,"could not parse track ID");
 			if(i < 256)
-				trk_list[i++] = trk_id;
-			c = getc(f);
+				mml->trk_list[i++] = mml->trk_id;
+			c = getc(mml->file);
 		}
-		trk_count = i;
-		last_cmd = parse_trk_list;
+		mml->trk_count = i;
+		mml->last_cmd = parse_trk_list;
 	}
 	else if(c == '#' || c == '@')
 	{
-		my_ungetc(c, f);
-		fgets(buffer,1024,f);
-		WARN("meta commands unhandled right now");
+		my_ungetc(c, mml);
+		fgets(mml->buffer,1024,mml->file);
+		WARN(mml, "meta commands unhandled right now");
 		return;
 	}
-	else if(iseol(c))
+	else if(iseol(mml, c))
 		return;
 
 	if(isblank(c))
 	{
 		// skip all non blank characters
-		c = my_getc(f);
-		my_ungetc(c, f);
+		c = my_getc(mml);
+		my_ungetc(c, mml);
 
-		if(iseol(c))
+		if(iseol(mml, c))
 			return;
-		if(last_cmd)
+		if(mml->last_cmd)
 		{
-			last_cmd(); // also responsible for advancing to the next line
+			mml->last_cmd(); // also responsible for advancing to the next line
 			return;
 		}
 	}
 
 	// nothing found, finish this line
-	while(!iseol(c))
-		c = getc(f);
+	while(!iseol(mml, c))
+		c = getc(mml->file);
 }
 
 //struct song* song_convert_mml(char* filename)
 int song_convert_mml(struct song* s, char* filename)
 {
-	fn = filename;
-	f = fopen(filename, "r");
-	if(!f)
+	struct mml_file *mml;
+	mml = calloc(1,sizeof(*mml));
+	mml->song = s;
+	mml->filename = filename;
+	mml->file = fopen(filename, "r");
+	mml->last_cmd = NULL;
+	mml->trk_count = 0;
+	mml->line = 0;
+
+	if(!mml->file)
 	{
 		perror(filename);
 		exit(-1);
 	}
+	while(!feof(mml->file))
+		parse_line(mml);
 
-	song = s;
-
-	last_cmd = NULL;
-	trk_count = 0;
-	line = 0;
-
-	while(!feof(f))
-		parse_line();
-
+	fclose(mml->file);
+	free(mml);
 	return 0;
 }
